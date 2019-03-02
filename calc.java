1ba8438f28036dc7f33aab585fd745893693db0e
greentea774 <vortex774@gmail.com>
2019-03-02 18:28:51 +0900

    コードフォーマット

--- calc.java
+++ calc.java
@@ -1,9 +1,9 @@
 public class calc {
     public static void main(String[] args) {
-         int a = 5;
-         int b = 6;
+        int a = 5;
+        int b = 6;
 
-         System.out.print("I have " + a + " orages\n");
-         System.out.print("I have " + b + " apples\n");
+        System.out.print("I have " + a + " orages\n");
+        System.out.print("I have " + b + " apples\n");
     }
 }
