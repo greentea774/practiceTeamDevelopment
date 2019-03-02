a2a97db74a75030344ec7cb7d779f06383d1aadb
greentea774 <vortex774@gmail.com>
2019-03-02 18:07:49 +0900

    gitignoreを追加＆計算クラスを追加

---
+++ calc.java
@@ -0,0 +1,9 @@
+public class calc {
+    public static void main(String[] args) {
+         int a = 5;
+         int b = 6;
+
+         System.out.print("I have " + a + " orages\n");
+         System.out.print("I have " + b + " apples\n");
+    }
+}
